e1a724f563f508023a7820b7a3bed1dfc5cbd5be
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-09-08 19:56:26 -0400

    Added object elements for returned data and missing commands.

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ const superagent = require('superagent'), nocache = require('superagent-no-cache
 const LM_GETUSERINFO = 'http://live.ksmobile.net/user/getinfo',
 	LM_GETVIDEOINFO = 'http://live.ksmobile.net/live/queryinfo',
 	LM_GETREPLAYVIDEOS = 'http://live.ksmobile.net/live/getreplayvideos',
-	LM_KEYWORDSEARCH = 'http://live.ksmobile.net/search/searchkeyword';
+	LM_KEYWORDSEARCH = 'http://live.ksmobile.net/search/searchkeyword',
+	LM_GETLIVEUSERS = 'https://live.ksmobile.net/live/newmaininfo';
 
 /*
 	Local Functions
@@ -56,7 +57,7 @@ module.exports = {
 			return httpGet(LM_GETUSERINFO, { userid: uid })
 		})
 			.then(data => {
-				return data; // TODO: return an actual user object
+				return data.user_info; 
 			});
 	},
 
@@ -87,7 +88,7 @@ module.exports = {
 			return httpGet(LM_GETVIDEOINFO, { userid: 0, videoid: vid })
 		})
 		.then(data => {
-			return data; // TODO: return an actual video object
+			return data; 	// Returns video_info & user_info objects for the video
 		});
 	},
 
@@ -126,7 +127,7 @@ module.exports = {
 		}).then(() => {
 			return httpGet(LM_GETREPLAYVIDEOS, { userid: uid, page_size: page, page_index: count });
 		}).then(data => {
-			return data; // TODO: return array of Video objects
+			return data.video_info; 
 		});
 	},
 
@@ -143,6 +144,37 @@ module.exports = {
 				return null;
 			});
 	},
+
+
+
+	/*
+		query: (u)rl for the chat json file
+
+		Returns: An array of message entries
+	*/
+	getChatHistoryForVideo(u) {
+
+		/* 	actual return is not under variable.data but rather variable and 
+		 	must be split into array:
+		
+		  	OLD CODE:
+
+		  	var messageList = [];
+			var split = e.split('\n');
+
+			for (var i = 0; i < split.length; i++) {
+				try {
+					var lineObj = JSON.parse(split[i]);
+					messageList.push(lineObj);
+				} catch (er) {
+					// just ignore it, sometimes it returns malformed json
+				}
+			}
+		*/
+
+	},
+
+
 
 	/*
 		query: string
@@ -178,162 +210,94 @@ module.exports = {
 			return httpGet(LM_KEYWORDSEARCH, { userid: uid, page_index: page, page_size: count, type: type, countryCode: country });
 		}).then(data => {
 			if (type == 1) { // list of users
-				return data; // TODO: return array of User objects
+				return data.data_info; 
 			} else if (type == 2) { // Video #tag search.
-				return data; // TODO: return array of Video objects
+				return data.data_info; 
 			}
 		});
 
 	},
 
-	getLive: function () {
 
-	}
+	/*
+		page: number (default 1)
+		count: number (default 1)
+	*/
+	getLive: function (page, count) {
+		return new Promise((resolve, reject) => {
+			if (typeof page == 'undefined' || page == null) { page = 1; }
+			if (typeof count == 'undefined' || count == null) { count = 10; }
 
-};
+			if (page <= 0) {
+				return reject('Page must be greater than 0');
+			}
 
-
-
+			if (count <= 0) {
+				return reject('Count must be greater than 0');
+			}
+			return resolve();
+		}).then(() => {
+			return httpGet(LM_GETLIVEUSERS, { page_index: page, page_size: count });
+		}).then(data => {
+			return data.video_info; 
+		});
 
 
 
 /*
 
-function _dosearch(showOverlay=true) {
+	
+	This is the actual OLD command for doing it.  Yes it had an option to filter by level.  I rather pass ALL 
+	of the data out and let that filtering (if desired) be done by the other end of the code.
 
-	if (showOverlay) {
-		$('#overlay .status').html('<progress></progress><br>Found '+(return_data.length)+' matching query...');
+
+	$.ajax({
+		url: 'https://live.ksmobile.net/live/newmaininfo',
+		data: {
+			page_size: 25,
+			page_index: current_page
+		},
+		cache: false,
+		type: 'GET',
+		dataType: 'json',
+		success: function(r){
+
+			var max = $('#limit').val();
+			if (r.data.video_info.length < max) max = r.data.video_info.length;
+
+			for (index = 0; index < max; index++) {
+				var entry = r.data.video_info[index];
+				
+				var level = parseInt(entry.level);
+				if ((level > $('#min').val()) && (level < $('#max').val())) {
+					count++;
+					var h = '<div class="entry '+(entry.sex==0?'female':'male')+'"><img src="'+entry.videocapture+'">';
+					h += '<h1>'+(entry.title.length > 0 ? entry.title : '-')+'</h1><h2><span>'+entry.uname+'</span></h2>';
+					h += '<h2 class="userid">User ID: <span>'+entry.userid+'</span></h2>';
+					h += '<h3>Level: <span>'+entry.level+'</span>Views: <span>'+entry.watchnumber+'</span></h3>';
+					h += '<h3>Country: <span>'+entry.countryCode+'</span>Likes: <span>'+entry.likenum+'</span></h3>';
+					h += '</div>';
+					$('#main').append(h);
+
+				}
+			}
+
+			if ((current_page < 100) && (count < $('#limit').val() )) {
+				current_page++;
+				setTimeout(function(){
+					doSearch();
+				}, 100);
+			}
+
+		}
+	});
+
+*/	
+
+
+
+
 	}
 
-	$.ajax({
-		url: 'http://live.ksmobile.net/search/searchkeyword',
-		data: {
-			keyword: encodeURI(query),
-			page_size: PAGE_SIZE,
-			page_index: page_index,
-			type : searchType
-		},
-		cache: false,
-		type: "GET",
-		dataType: "json",
-		timeout: 15000,
-		error: function(e){
-			callback_holder(return_data);
-		},
-		success: function(e) {
+};
 
-			if (e.data.data_info.length == 0) {
-				callback_holder(return_data);
-				return;
-			} else {
-				for (i = 0; i < e.data.data_info.length; i++) {
-					// Only push ones that are defined.
-					if (typeof e.data.data_info[i] != 'undefined') {
-
-						if (searchType == 1) {
-							return_data.push({
-								userid : e.data.data_info[i].user_id
-							});
-						} else {
-							return_data.push(e.data.data_info[i]);
-						}
-					}
-				}
-
-				// Even with this code is here it still will only return 10 results max.
-				if (e.data.data_info.length == PAGE_SIZE) {
-					page_index++;
-					_dosearch();
-				} else {
-					index = 0;
-					if (searchType == 1) {
-						_dosearch2();
-					} else {
-						callback_holder(return_data);
-					}
-				}
-			}
-		}
-	});
-
-
-}
-
-function _dosearch2() {
-
-	$('#overlay .status').html('<progress min="0" max="'+return_data.length+'" value="'+index+'"></progress><br>Getting info on '+return_data.length+' users...');
-	$.ajax({
-		url: 'http://live.ksmobile.net/user/getinfo',
-		data: {
-			userid: return_data[index].userid
-		},
-		cache: false,
-		type: "GET",
-		dataType: "json",
-		timeout: 15000,
-		error: function(e){
-			callback_holder(return_data);
-		},
-		success: function(e) {
-			return_data[index] = {
-					userid: e.data.user.user_info.uid,
-					nickname: e.data.user.user_info.nickname,
-					sex: e.data.user.user_info.sex < 0 ? '' : ( e.data.user.user_info.sex == 0 ? 'female' : 'male'),
-					face: e.data.user.user_info.face,
-					level: parseInt(e.data.user.user_info.level),
-					followings: parseInt(e.data.user.count_info.following_count),
-					fans: parseInt(e.data.user.count_info.follower_count),
-					videos : [],
-					videosplus : false
-			};
-			
-
-			index++;
-			if (index < return_data.length) {
-				_dosearch2();
-			} else {
-				index = 0;
-				// We return data instead of getting videos
-				callback_holder(return_data);
-			}
-
-		}
-	});
-}
-
-
-
-
-
-function getChat(u, cb) {
-	callback_holder = cb;
-	
-	$.ajax({
-		url: u,
-		cache: false,
-		type: "GET",
-		dataType: "text",
-		timeout: 15000,
-		error: function(e) {
-			callback_holder(false);
-		},
-		success: function(e) {
-			var messageList = [];
-			var split = e.split('\n');
-
-			for (var i = 0; i < split.length; i++) {
-				try {
-					var lineObj = JSON.parse(split[i]);
-					messageList.push(lineObj);
-				} catch (er) {
-					// just ignore it, sometimes it returns malformed json
-				}
-			}
-
-			callback_holder(messageList);
-		}
-	});
-}
-
-
-*/
