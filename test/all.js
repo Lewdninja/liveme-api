bd33d83c1f11f211e69b30e7727a75b8304119a5
thecoder75 <30754178+thecoder75@users.noreply.github.com>
2017-09-08 22:23:40 -0400

    Added tests so Travis will pass.

--- test/all.js
+++ test/all.js
@@ -1,10 +1,38 @@
 // all.js
 
-const livemeapi = require('../index');
+var livemeapi = require('../index'), return_code = 0;
+
 
 // LiveMe Academy: 828324394803003392
 var userInfo = livemeapi.getUserInfoSync('828324394803003392');
+userInfo.then((res) => {
+	if (res.user.user_info.uid != '828324394803003392') {
+		return_code = 1;
+	} else {
+		console.log('getuserInfoSync passed, got user details back.');		
+	}
+});
+
+// Test Video: 15015379476728257645 - Live.me Broadcaster Adademy #training
+console.log('\nChecking getVideoInfoSync...');
+var videoInfo = livemeapi.getVideoInfoSync('15015379476728257645');
+videoInfo.then((res) => {
+	if (res.video_info.vid != '15015379476728257645') {
+		return_code = 1;
+	} else {
+		console.log('getVideoInfoSync passed, got video details back.');
+	}
+});
+
+// LiveMe Academy: 828324394803003392
+var replays = livemeapi.getUserReplaysSync('828324394803003392', 1, 2);
+replays.then((res) => {
+	if (res.video_info.length == 0) {
+		return_code = 1;
+	} else {
+		console.log('getUserReplaysSync passed, got an array of replays.');
+	}
+});
 
 
-
-return 0;
+return return_code;
